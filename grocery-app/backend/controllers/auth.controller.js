0b07244e9a23fb3068086dd2ffc062dffe5b8b6c
shahbaz-riaz <muhammad.shahbazt20@gmail.com>
2025-08-12 14:50:57 +0500

    modified auth controller

--- grocery-app/backend/controllers/auth.controller.js
+++ grocery-app/backend/controllers/auth.controller.js
@@ -8,6 +8,28 @@ import bcrypt from 'bcrypt'
 export const signUp = async (req, res) => {
   const session = await mongoose.startSession()
   session.startTransaction()
+  try {
+    const {name,email,password} = req.body
+    
+    // check if user already exists
+    const existingUser = await User.findOne({ email })
+
+    if( existingUser ) {
+      return res.status(400).json({ message: 'User already exists' })
+    }
+
+    const user= {name,email,password}
+    await user.save({ session })
+
+
+    await session.commitTransaction()
+    session.endSession()
+
+  } catch (error) {
+    await session.abortTransaction()
+    session.endSession()
+    return res.status(500).json({ message: 'Internal server error' })
+  }
 
 }
 
