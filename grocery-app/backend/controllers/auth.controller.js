9262a315c85a882675a2b263f4fb1b9c08161819
shahbaz-riaz <muhammad.shahbazt20@gmail.com>
2025-08-16 02:06:22 +0500

    first commmit

--- grocery-app/backend/controllers/auth.controller.js
+++ grocery-app/backend/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose'
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
+import User from '../models/user.model.js'
 
 
 
@@ -18,8 +19,10 @@ export const signUp = async (req, res) => {
       return res.status(400).json({ message: 'User already exists' })
     }
 
-    const user= {name,email,password}
-    await user.save({ session })
+    const newUser = new User({
+      name,email,password
+    })
+    await User.save({ session })
 
 
     await session.commitTransaction()
@@ -33,9 +36,22 @@ export const signUp = async (req, res) => {
 
 }
 
-export const signIn = async (req, res) => {}
+export const signIn = async (req, res) => {
+  try {
+    const {email,password} = req.body
+    const user = await User.findOne({email})
+    if(!user){
+      return res.status(401).json({message:'invalid credentials'})
+    }
+  } catch (error) {
+    console.log(error)
+    return res.send('error occur during signin')
+  }
+}
 
-export const signOut = async (req, res) => {}
+export const signOut = async (req, res) => {
+  
+}
 
 
 
